4f4f794b9379cb06ae86bd30c8a50e74cfc883f0
Julia <piorek.julia@gmail.com>
2020-12-21 14:45:17 +0100

    saving work

--- script.js
+++ script.js
@@ -5,51 +5,11 @@ function showNav() {
     } else {
         x.className = "hero";
     }
-
 }
 
 function hideNav() {
     const element = document.getElementById("hero");
     element.classList.remove("responsive");
-
-}
-
-// function showContent() {
-//     const x = document.getElementById("section");
-//     if (x.classList.contains("section-style")) {
-//         x.classList.add(" desktop");
-//     } else {
-//         x.className = "section-style"
-//     }
-// }
-
-// function showContent() {
-//     let x = document.querySelectorAll('.section-style');
-//     for (let i = 0; i < x.length; i++) {
-//         x[i].classList.add('desktop');
-//     }
-// }
-
-function showTherapy1() {
-    const x = document.querySelector('.therapy1');
-    x.classList.toggle('desktop');
-}
-
-function showTherapy2() {
-    const x = document.querySelector('.therapy2');
-    x.classList.toggle('desktop');
-}
-
-function showAbout() {
-    const x = document.querySelector('.about');
-    x.classList.toggle('desktop');
-}
-
-
-document.getElementById("sticky").addEventListener("click", hideSticky);
-
-function hideSticky() {
-    document.getElementById("sticky").style.display = "none";
 }
 
 document.addEventListener("scroll", showSticky);
@@ -58,6 +18,14 @@ function showSticky() {
     document.getElementById("sticky").style.display = "block";
 }
 
+// document.getElementById("sticky").addEventListener("click", hideSticky);
+//
+// function hideSticky() {
+//     document.getElementById("sticky").style.display = "none";
+// }
+//https://developer.mozilla.org/en-US/docs/Web/API/Window/pageYOffset
 
-
-
+function showContent(sectionName) {
+    const x = document.querySelector('.' + sectionName);
+    x.classList.toggle('desktop');
+}
