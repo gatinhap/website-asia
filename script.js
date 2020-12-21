ff1063319e5478da6ee96c9458196c1e0abd1cbe
Julia <piorek.julia@gmail.com>
2020-12-21 18:13:48 +0100

    saving work

--- script.js
+++ script.js
@@ -12,18 +12,13 @@ function hideNav() {
     element.classList.remove("responsive");
 }
 
-document.addEventListener("scroll", showSticky);
+document.addEventListener("scroll", showScroll);
 
-function showSticky() {
-    document.getElementById("sticky").style.display = "block";
+function showScroll() {
+    if (window.pageYOffset >= 200) {
+        document.getElementById("sticky").style.display = "block";
+    } else document.getElementById("sticky").style.display = "none";
 }
-
-// document.getElementById("sticky").addEventListener("click", hideSticky);
-//
-// function hideSticky() {
-//     document.getElementById("sticky").style.display = "none";
-// }
-//https://developer.mozilla.org/en-US/docs/Web/API/Window/pageYOffset
 
 function showContent(sectionName) {
     const x = document.querySelector('.' + sectionName);
